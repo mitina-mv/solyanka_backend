18529b27b7a3ecd2224a513eb929f39d65dd6bcf
lolipopmari <mit.maria2019@yandex.ru>
2023-05-26 22:19:35 +0300

    feat: дописан crud на чаты добавлен софт делит

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ChatCreateRequest;
 use App\Models\Chat;
+use Exception;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response as HttpFoundationResponse;
 use Illuminate\Support\Facades\Auth;
@@ -15,10 +16,12 @@ class ChatController extends Controller
     public function list()
     {   
         $user = Auth::user();
-        $chats = Chat::where('user_id', $user->id)->get();
+        $chats = Chat::where('user_id', $user->id)
+                ->select('id', 'name', 'icon')
+                ->get()->all();
 
         return response()->json([
-            'chats' => $chats,
+            $chats,
         ], HttpFoundationResponse::HTTP_OK);
     }
 
@@ -27,12 +30,76 @@ class ChatController extends Controller
     {
         $chat = Chat::create([
             'name' => $request->name,
-            'icon' => $request->icon,
+            'icon' => Chat::PATH_ICON . $request->icon . '.png',
             'user_id' => Auth::user()->id
         ]);
 
         return response()->json([
-            'chat' => $chat,
+            $chat,
         ], HttpFoundationResponse::HTTP_OK);
+    }
+
+    public function read($id)
+    {
+        $chat = $this->getChat($id);
+
+        if(isset($chat['status']))
+        {
+            return response()->json($chat, HttpFoundationResponse::HTTP_BAD_REQUEST);
+        } 
+
+        return response()->json([
+            $chat,
+        ], HttpFoundationResponse::HTTP_OK);
+    }
+
+    public function destroy($id)
+    {
+        $chat = $this->getChat($id);
+
+        if(isset($chat['status']))
+        {
+            return response()->json($chat, HttpFoundationResponse::HTTP_BAD_REQUEST);
+        }
+
+        try {
+            $chat->forceDelete();
+        } catch (Exception $e) {
+            $chat->delete();
+        }
+
+        return response()->json([
+            'id' => $id,
+            'message' => 'Удаление успешно'
+        ], HttpFoundationResponse::HTTP_OK);
+    }
+
+    private function getChat($id)
+    {
+        $user = Auth::user();
+        
+        if($user)
+        {
+            $chat = Chat::where([
+                'user_id' => $user->id,
+                'id' => $id
+            ])->first();
+    
+            if(!$chat)
+            {
+                return [
+                    'status' => 'error',
+                    'message' => "Чата с указанным ID не существует"
+                ]; 
+            }
+
+            return $chat;
+        } else {
+            return [
+                'status' => 'error',
+                'message' => "Пользователь не авторизован, мы пока не знаем, что с этим делать"
+            ]; 
+        }
+
     }
 }
