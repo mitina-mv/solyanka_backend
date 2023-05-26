c64d18829b81b148d53179bf9ba3eb4db96a5dd6
lolipopmari <mit.maria2019@yandex.ru>
2023-05-26 17:04:34 +0300

    feat: routes chat

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'chat'], function() {
+    Route::get('/', [ChatController::class, 'list']);
+    Route::get('/create', [ChatController::class, 'create']);
+    Route::get('/{id}', [ChatController::class, 'read']);
+    // Route::post('/{id}', [ChatController::class, 'update']);
+    Route::delete('/{id}', [ChatController::class, 'destroy']);
+});
