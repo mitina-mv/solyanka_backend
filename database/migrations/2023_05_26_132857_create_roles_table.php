e69b6f5460d1466dd264d700475c6ed0c2614ec1
lolipopmari <mit.maria2019@yandex.ru>
2023-05-26 16:34:18 +0300

    feat: role model + migration

---
+++ database/migrations/2023_05_26_132857_create_roles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('user_id')
+                ->nullable()
+                ->constrained('users');
+            $table->string('text');
+            $table->string('icon');
+            $table->boolean('isChoice');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('roles');
+    }
+};
