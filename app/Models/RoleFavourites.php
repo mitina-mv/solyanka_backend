6cb936462429ecfcaa49b1e83b823b6c112a3bc8
lolipopmari <mit.maria2019@yandex.ru>
2023-05-26 23:20:15 +0300

    feat: добавлена таблица, модель

--- app/Models/RoleFavourites.php
+++ app/Models/RoleFavourites.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Request extends Model
+class RoleFavourites extends Model
 {
     use HasFactory;
+    
+    public $timestamps = false;
 
     protected $fillable = [
         'name',
