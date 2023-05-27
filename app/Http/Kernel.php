f04cd8f3eb9a91b25f8fc562b9a23edfded623d8
lolipopmari <mit.maria2019@yandex.ru>
2023-05-27 15:45:14 +0300

    feat: cors delete

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\AddCorsHeaders;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -35,6 +36,7 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
+            AddCorsHeaders::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
